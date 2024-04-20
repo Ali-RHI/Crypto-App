2d2f7be3bb6e2be18b87b962191adf88019317e8
Ali Rouhi <alirhtg@gmail.com>
2024-04-20 17:50:38 +0330

    pagination functionality

--- src/templates/HomePage.jsx
+++ src/templates/HomePage.jsx
@@ -7,21 +7,26 @@ import Pagination from '../modules/Pagination.jsx';
 function HomePage() {
 	const [coins, setCoins] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
+	const [page, setPage] = useState(1);
 
 	useEffect(() => {
+        setIsLoading(true);
 		const getData = async () => {
-			const res = await fetch(getCoinsList());
+			const res = await fetch(getCoinsList(page));
 			const json = await res.json();
 			setCoins(json);
 			setIsLoading(false);
 		};
 
 		getData();
-	}, []);
+	}, [page]);
 
 	return (
 		<>
-			<Pagination />
+			<Pagination
+				page={page}
+				setPage={setPage}
+			/>
 			<TableCoin
 				coins={coins}
 				isLoading={isLoading}
