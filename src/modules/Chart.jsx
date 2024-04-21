808f9bcfc98c2d7d3c87606f6255e935794b289a
Ali Rouhi <alirhtg@gmail.com>
2024-04-21 10:06:35 +0330

    writing helper functions to get data needed to draw chart for coins

--- src/modules/Chart.jsx
+++ src/modules/Chart.jsx
@@ -1,10 +1,16 @@
+import { useState } from 'react';
 import styles from './Chart.module.css';
 
 function Chart({ chart, setChart }) {
+	const [type, setType] = useState('prices');
 	return (
 		<div className={styles.container}>
-			<span className={styles.cross} onClick={() => setChart(null)}>X</span>
-            <div className={styles.chart}></div>
+			<span
+				className={styles.cross}
+				onClick={() => setChart(null)}>
+				X
+			</span>
+			<div className={styles.chart}></div>
 		</div>
 	);
 }
