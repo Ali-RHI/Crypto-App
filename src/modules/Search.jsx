a573695244b2ccba4cdfeeb0eaa77dac1c9699d9
Ali Rouhi <alirhtg@gmail.com>
2024-04-21 05:05:52 +0330

    search box functionality

--- src/modules/Search.jsx
+++ src/modules/Search.jsx
@@ -1,14 +1,19 @@
 import { useEffect, useState } from 'react';
 import { searchCoin } from '../services/cryptoAPI.js';
+import { RotatingLines } from 'react-loader-spinner';
 
 function Search({ currency, setCurrency }) {
 	const [text, setText] = useState('');
 	const [coins, setCoins] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
 
 	useEffect(() => {
 		const controller = new AbortController();
 
-		if (!text) return;
+		if (!text) {
+			setIsLoading(false);
+			return;
+		}
 
 		const search = async () => {
 			try {
@@ -18,6 +23,7 @@ function Search({ currency, setCurrency }) {
 				const json = await res.json();
 				console.log(json);
 				if (json.coins) {
+					setIsLoading(false);
 					setCoins(json.coins);
 				} else {
 					alert(json.status.error_message);
@@ -28,6 +34,7 @@ function Search({ currency, setCurrency }) {
 				}
 			}
 		};
+		setIsLoading(true);
 		search();
 
 		return () => controller.abort();
@@ -50,6 +57,27 @@ function Search({ currency, setCurrency }) {
 				<option value="eur">EUR</option>
 				<option value="jpy">JPY</option>
 			</select>
+			<div>
+				{isLoading && (
+					<RotatingLines
+						width="50px"
+						height="50px"
+						strokeWidth="2"
+						strokeColor="#3874ff"
+					/>
+				)}
+				<ul>
+					{coins.map((coin) => (
+						<li key={coin.id}>
+							<img
+								src={coin.thumb}
+								alt={coin.name}
+							/>
+							<p>{coin.name}</p>
+						</li>
+					))}
+				</ul>
+			</div>
 		</div>
 	);
 }
